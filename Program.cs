5909aa490a52bd3c80e4a15340750a1796b2521f
Rick Arcanjo <Ricardo.cassettari07@gmail.com>
2023-03-10 13:57:06 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Adivinha");
+Console.WriteLine();
+
+Console.WriteLine("O que é, o que é...");
+Console.WriteLine("... passa diante do sol e não faz sombra?\n");
+Thread.Sleep(5000);
+
+Console.WriteLine("O vento");
